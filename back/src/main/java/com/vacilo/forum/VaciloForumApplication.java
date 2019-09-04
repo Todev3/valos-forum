d5ed78d243902f8f6e849ea46fb0bb0144c2c7df
Todev3 <emerson_abedias@hotmail.com>
2019-09-03 21:56:52 -0300

    roca kotlin por Java 12

---
+++ back/src/main/java/com/vacilo/forum/VaciloForumApplication.java
@@ -0,0 +1,13 @@
+package com.vacilo.forum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaciloForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaciloForumApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/vacilo/forum/VaciloForumApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vacilo.forum;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VaciloForumApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
